4354b82ef65514f939d7bc72723cda040bea4957
Pedro <pedro.saravia.q@uni.pe>
2022-09-08 15:47:38 -0500

    set axios default url

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,12 @@
-const { Router } = require('express');
+const { Router } = require("express");
+const axios = require("axios");
+axios.defaults.baseURL = "https://api.rawg.io/api";
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
 
 module.exports = router;
