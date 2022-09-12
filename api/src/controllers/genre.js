a316eef20462ded291bbc84a4ddc5c0fbe230281
Pedro <pedro.saravia.q@uni.pe>
2022-09-12 18:38:15 -0500

    updated getGenres route comments

--- api/src/controllers/genre.js
+++ api/src/controllers/genre.js
@@ -8,10 +8,12 @@ module.exports = {
     try {
       // retrieves the genres from the API
       const apiGenres = (await axios.get(`/genres?key=${API_KEY}`)).data.results;
+      //
       // tries to insert the genres in the database, then sends them all
       const createGenres = ({ name }) => Genre.findOrCreate({ where: { name } });
       await Promise.all(apiGenres.map(createGenres));
       res.send(await Genre.findAll({ attributes: ["name"] }));
+      //
     } catch (err) {
       console.log(err.message);
       res.status(400).send(err);
